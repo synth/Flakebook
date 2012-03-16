c6ff0e39f54b15930d414ed7cf3aa2cfa8975cb8
Peter P <pete@p373.net>
2012-03-16 05:28:50 -0400

    starting work with koala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < ActionController::Base
-  include Facebooker2::Rails::Controller
   protect_from_forgery
+
+  before_filter :parse_facebook_cookies
+  helper_method :current_facebook_user
+  def parse_facebook_cookies
+   @facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+  end
+
+  # def get_oath
+  #   oauth = Koala::Facebook::OAuth.new(app_id, app_secret, callback_url)
+  # end
+
+  def get_graph
+    @graph = Koala::Facebook::API.new(oauth_access_token)
+  end
+  
+  def current_facebook_user
+    # oauth = Koala::Facebook::OAuth.new(Facebook::APP_ID, Facebook::SECRET, Facebook::CALLBACK_URL)
+    nil
+  end
+
+  def current_facebook_client
+  end
+
   def ensure_authenticated
     if current_facebook_user.nil?
       redirect_to login_url
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def fb_login_and_redirect(url, options = {})
+    # Check if we got the update_page method (pre-Rails 3.1)
+    if respond_to? 'update_page'
+      js = update_page do |page|
+        page.redirect_to url
+      end
+    # Else use plain js
+    else
+      js = "window.location.href = '#{url}'"
+    end
+    text = options.delete(:text)
+  
+    #rails 3 only escapes non-html_safe strings, so get the raw string instead of the SafeBuffer
+    content_tag("fb:login-button",text,options.merge(:onlogin=>js.to_str))
+  end
+
+  def fb_logout_link(text,url,*args)
+    function= "FB.logout(function() {window.location.href = '#{url}';})"
+    link_to_function text, function.to_str, *args
+  end  
 end
