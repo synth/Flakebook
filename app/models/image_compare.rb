372a3a7626125ac25669ce417e78cbf6d1eb3a64
Peter P <pete@p373.net>
2012-04-02 04:39:32 +0900

    proper image comparison algorithm

--- app/models/image_compare.rb
+++ app/models/image_compare.rb
@@ -1,7 +1,8 @@
+require 'RMagick'
 class ImageCompare
   attr_accessor :img1, :img2
 
-  @@image_base = "/public"
+  @@image_base = "/public/"
 
   def initialize(img1, img2)
     @img1 = img1
@@ -9,12 +10,12 @@ class ImageCompare
   end
   
   def difference
-    debugger
     mimg1 = Magick::ImageList.new(Rails.root.to_s+@@image_base+@img1)
     mimg2 = Magick::ImageList.new(Rails.root.to_s+@@image_base+@img2)
-    diff = mimg1.difference(ming2)
-    avg_diff = (diff[0]+diff[1]+diff[2])/3
-    return true
+    diff = mimg1.difference(mimg2.scale(mimg1.columns, mimg1.rows))
+    #avg_diff = (diff[0]+diff[1]+diff[2])/3
+    avg_diff = diff
+    return avg_diff 
   end
   
   def same_image?
