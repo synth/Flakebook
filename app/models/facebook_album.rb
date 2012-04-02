c33b80b5fdc733d846d026c5a2ccd9cb094bdaa5
Peter P <pete@p373.net>
2012-04-02 05:37:44 -0400

    fixed bug with album id

--- app/models/facebook_album.rb
+++ app/models/facebook_album.rb
@@ -1,9 +1,11 @@
 class FacebookAlbum
-  attr_accessor :album, :photos
+  attr_accessor :album, :photos, :id
   def initialize(album, client)
     @client = client
     # @album = Mogli::Album.find(album_id, client)
     @album = album
     @photos = @album.photos
+    @id = @album.id
   end
+  
 end
